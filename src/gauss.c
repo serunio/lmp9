4be3611267e463f85da611b13f254f6f8d0d7a28
serunio <jak.haraszkiewicz@gmail.com>
2023-12-22 00:50:07 +0100

    dodanie zamiany do gauss.c

--- src/gauss.c
+++ src/gauss.c
@@ -1,6 +1,7 @@
 #include "gauss.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include "zamiana.h"
 
 /**
  * Zwraca 0 - elimnacja zakonczona sukcesem
@@ -17,17 +18,18 @@ int eliminate(Matrix *mat, Matrix *b) {
     double *wspolczynnik;
     //
     for (i = 0; i < mat->c - 1; i++) {
+        zamiana(mat, b, i);
         int x = 0;
         wspolczynnik = malloc(sizeof(double) * (mat->r - 1 - i));
         for (j = i + 1; j < mat->r; j++) {
-
+            zamiana(mat, b, i);
             wspolczynnik[x] = (mat->data[j][i]) / (mat->data[i][i]);
             if (wspolczynnik[x] == 0)
                 return 1;
 
             for (l = i; l < mat->c; l++) {
                 mat->data[j][l] = (mat->data[j][l]) - (mat->data[i][l] * wspolczynnik[x]);
-                printf("mat data od j od l %f\n", mat->data[j][l]);
+                //printf("mat data od j od l %f\n", mat->data[j][l]);
             }
             b->data[j][0] = (b->data[j][0]) - (b->data[0][0] * wspolczynnik[x]);
             x++;
